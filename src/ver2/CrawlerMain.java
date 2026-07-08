ed09d452008721329f40e20253dc6e890c04c0e4
kawashun <shunsakukawakami0613@gmail.com>
2026-07-08 15:14:20 +0000

    update

--- src/ver2/CrawlerMain.java
+++ src/ver2/CrawlerMain.java
@@ -5,28 +5,42 @@ import java.nio.file.Paths;
 
 public class CrawlerMain {
 
-    static String downloadUrl = "https://www.rakuten.co.jp/";
+    static String targetUrl = "https://www.rakuten.co.jp/";
     // static String downloadUrl = "https://books.toscrape.com/";
 
-    static Path currentPath = Paths.get("").toAbsolutePath();
-
-    static int depth = 1;
+    static int maxDepth = 1;
     
     public static void main(String[] args){
         
-        MakeFolder makeFolder = new MakeFolder();
+        // クロール用の保存フォルダを指すパスの作成
+        Path folderPath = makeCrawlFolderPath();
         
+        int currentDepth = 1;
+
+        // クロール
+        Crawler crawler = new Crawler(folderPath);
+        crawler.crawl(targetUrl, currentDepth, maxDepth);
+    }
+    
+
+    // ダウンロードするフォルダの作成
+    private static Path makeCrawlFolderPath(){
+        
+        // カレントリのパス
+        Path currentPath = getCurrentPath();
+
+        // targetUrlをフォルダ名に使用する
         ReplaceCannotUseWord replaceCannotUseWord = new ReplaceCannotUseWord();
-        String replacedDownloadUrl = replaceCannotUseWord.replace(downloadUrl);
-        Path urlNameFolder = currentPath.resolve("download").resolve(replacedDownloadUrl);
-        makeFolder.make(urlNameFolder);
+        String replacedTargetUrl = replaceCannotUseWord.replace(targetUrl);
         
-        Crawler crawler = new Crawler(downloadUrl, depth, urlNameFolder);
-        try{
-            crawler.crawl();
-        }catch(Exception e){
-            System.out.println(e);
-        }
+        // 保存するフォルダのパスを作成
+        return currentPath.resolve("download").resolve(replacedTargetUrl);
     }
 
+
+    private static Path getCurrentPath(){
+        // カレントリのパスを取得
+        return Paths.get("").toAbsolutePath();
+    }
+    
 }
